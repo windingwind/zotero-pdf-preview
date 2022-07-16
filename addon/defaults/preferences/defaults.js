e35f65b25135ce6b93d90bdd12bade356a336146
xiangyu <3170102889@zju.edu.cn>
2022-07-17 02:19:54 +0800

    add: double-click to jump/open PDF
    fix: show annotation bug

--- addon/defaults/preferences/defaults.js
+++ addon/defaults/preferences/defaults.js
@@ -1,6 +1,8 @@
 pref("extensions.zotero.pdfpreview.enable", true);
 pref("extensions.zotero.pdfpreview.autoPreview", false);
-pref("extensions.zotero.pdfpreview.showAnnotations", false);
+pref("extensions.zotero.pdfpreview.showAnnotations", true);
+pref("extensions.zotero.pdfpreview.showHover", true);
+pref("extensions.zotero.pdfpreview.enableJump", true);
 pref("extensions.zotero.pdfpreview.darkMode", false);
 pref("extensions.zotero.pdfpreview.previewPageNum", "10");
 pref("extensions.zotero.pdfpreview.previewTabName", "Preview");
